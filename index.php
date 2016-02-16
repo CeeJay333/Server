4d2a689fb37eea4afc97d06ed55567607870203e
CeeJay333 <carstenjunk@msn.com>
2016-02-16 12:54:21 +0000

    .

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <body bgcolor="black">
-<h2 align="center"><font color="white">Die Seite befindet sich im wiederaufbau...</font></h2>
+<h2 align="center"><font color="white">Die Seite befindet sich im Wiederaufbau...</font></h2>
 </body>
 </html>
